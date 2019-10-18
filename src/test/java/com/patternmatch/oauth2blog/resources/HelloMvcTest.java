c40e06b7e5480c3d28d17065bad1e61670118774
Alexandr Shevtsov <ashevtsov@luxoft.com>
2019-10-18 16:56:43 +0300

    integration tests

--- src/test/java/com/patternmatch/oauth2blog/resources/HelloMvcTest.java
+++ src/test/java/com/patternmatch/oauth2blog/resources/HelloMvcTest.java
@@ -1,36 +1,50 @@
 package com.patternmatch.oauth2blog.resources;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.patternmatch.oauth2blog.config.TestConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = TestConfig.class
-)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class HelloMvcTest {
+
+    private static final String TEST_USER_NAME = "test@test.com";
+    private static final String TEST_USER_PASSWORD = "tester";
+    private static final String USER_NAME_PARAM = "username";
+    private static final String USER_PASSWORD_PARAM = "password";
+    private static final String GRANT_TYPE_PARAM = "grant_type";
+    private static final String TEST_USER_GRANT_TYPE = "password";
+    private static final String OAUTH_TOKEN_URL = "/oauth/token";
+    private static final String HTTP_BASIC_USER_NAME = "my-client";
+    private static final String HTTP_BASIC_USER_PASSWORD = "my-secret";
+    private static final String CONTENT_TYPE_JSON = "application/json;charset=UTF-8";
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER = "Bearer ";
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @WithUserDetails("user@test.com")
     public void shouldAllowUserWithNoAuthorities() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/hello?name=Seb")
+        mockMvc.perform(get("/api/hello?name=Seb")
+                .header(AUTHORIZATION_HEADER, obtainAccessToken())
                 .accept(MediaType.ALL))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.greetings", is("Welcome Seb!")));
@@ -41,5 +55,26 @@ public class HelloMvcTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/hello?name=Seb")
                 .accept(MediaType.ALL))
                 .andExpect(status().isUnauthorized());
+    }
+
+    private String obtainAccessToken() throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add(USER_NAME_PARAM, TEST_USER_NAME);
+        params.add(USER_PASSWORD_PARAM, TEST_USER_PASSWORD);
+        params.add(GRANT_TYPE_PARAM, TEST_USER_GRANT_TYPE);
+
+        String resultString = mockMvc.perform(post(OAUTH_TOKEN_URL)
+                .params(params)
+                .with(httpBasic(HTTP_BASIC_USER_NAME, HTTP_BASIC_USER_PASSWORD))
+                .accept(CONTENT_TYPE_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(CONTENT_TYPE_JSON))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return BEARER + jsonParser.parseMap(resultString).get(ACCESS_TOKEN).toString();
     }
 }
